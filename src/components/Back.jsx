4c03ecdb34f1e6a8164b6d1ed6bc78d340589549
vmhowley <vmhowleyh@gmail.com>
2023-11-26 08:42:27 -0400

    Back fixed

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -11,8 +11,7 @@ function Back() {
           onClick={() => {
             navigate(-1, { replace: false });
           }}
-          className="flex cursor-pointer h-6 w-6 pl-6 pr-6"
-        >
+          className="flex cursor-pointer h-6 w-6 pl-6 pr-6">
           <ArrowBackRoundedIcon />
         </div>
     </div>

8fcf61955d9da7b984d913c79a51d6cf7b29cfba
vmhowley <vmhowleyh@gmail.com>
2023-11-26 08:41:12 -0400

    Back fixed

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
-
+import { useNavigate } from 'react-router-dom';
 
 function Back() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <div 

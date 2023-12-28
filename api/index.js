fb9b1cfdcb76825b8da6bc77cf66a5f46cf7c51f
Victor Manuel Morillo Howley <victormh@IBSYSTEMS.COM>
2023-12-28 11:45:54 -0400

    api

--- api/index.js
+++ api/index.js
@@ -1,21 +1,35 @@
 const { MongoClient } = require('mongodb');
 const Express = require('express');
-const app = Express();
 const uri ="mongodb://localhost:27017/";
 const client = new MongoClient(uri);
+const bodyParser = require('body-parser');
 
 var db;
+
+
+const app = Express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(9000, async ()=>{
   console.log('listening port 9000')
    await client.connect();
-    db =  client.db('local');
+    db =  client.db('allpaydb');
   console.log('Mongo Connected')
 })
 
-app.get('/1', async(req,res)=>{
-  const collection = await db.collection('startup_log');
-  // Find the first document in the collection
-  const first = await collection.find({}).toArray() 
-  res.send(first);
-})
 
+app.get('/', async(req,res)=>{
+  const collection = await db.collection('allpaycollection');
+  // Find the first document in the collection
+  const first = await collection.find({ id: 1
+  }).toArray() 
+  res.send(first);
+}) 
+app.post('/1', async(req,res)=>{
+  const collection = await db.collection('allpaycollection');
+  // Find the first document in the collection
+  const first = await collection.find({ id: 2
+  }).toArray() 
+  console.dir(req.body)
+  res.send('ready');
+})
